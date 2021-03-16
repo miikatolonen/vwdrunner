527cca4bdfb7106ab252f167c7c1eb4a8538ae37
Miika Tolonen <miikato@uef.fi>
2021-03-16 20:39:50 +0200

    temporary fix

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/app.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: './src/app.js'
+    filename: 'app.js'
   },
   devServer: {
     historyApiFallback: {
